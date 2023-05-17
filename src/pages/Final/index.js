af03da74a932c419a91494a9309a0852f67af0e6
Rodrigo Machado <rodrigoborgesmachado@gmail.com>
2023-05-17 13:12:51 -0300

    Alterando texto.

--- src/pages/Final/index.js
+++ src/pages/Final/index.js
@@ -10,17 +10,30 @@ function Final(){
         <div className='container'>
             <h2><b>🎉Parabéns {localStorage.getItem(configData.NOME_PARAM)}!!!🎉</b></h2>
             <br/>
-            <h3>
-                Você é um mestre da tabuada! 🧠💪<br/><br/>
-                ✅Você acertou {localStorage.getItem(configData.QUANTIDADE_ACERTOS)} de {localStorage.getItem(configData.QUANTIDADE_PARAM)} em tempo recorde! ⏱️⚡️<br/>
-                Você levou apenas {localStorage.getItem(configData.TEMPO_PARAM)} segundos! ⏱️<br/><br/>
-                Compartilhe sua conquista com seus amigos e desafie-os a superar seu desempenho!<br/><br/>
+            {
+                localStorage.getItem(configData.QUANTIDADE_ACERTOS) == localStorage.getItem(configData.QUANTIDADE_PARAM) ?
+                <h3>
+                    Você é um mestre da tabuada! 🧠💪<br/><br/>
+                    ✅Você acertou {localStorage.getItem(configData.QUANTIDADE_ACERTOS)} de {localStorage.getItem(configData.QUANTIDADE_PARAM)} em tempo recorde! ⏱️⚡️<br/>
+                    Você levou apenas {localStorage.getItem(configData.TEMPO_PARAM)} segundos! ⏱️<br/><br/>
+                    Compartilhe sua conquista com seus amigos e desafie-os a superar seu desempenho!<br/><br/>
 
-                🔥 Quão rápido você pode ser? Descubra em Tabuada Divertida! 💥
-            </h3>
+                    🔥 Quão rápido você pode ser? Descubra em Tabuada Divertida! 💥
+                </h3>
+                :
+                <h3>
+                    Não foi dessa vez, você ficou fora do ranking!!! 🧠💪<br/><br/>
+                    ✅Você acertou {localStorage.getItem(configData.QUANTIDADE_ACERTOS)} de {localStorage.getItem(configData.QUANTIDADE_PARAM)}! ⏱️⚡️<br/>
+                    Você levou apenas {localStorage.getItem(configData.TEMPO_PARAM)} segundos! ⏱️<br/><br/>
+                    Compartilhe sua conquista com seus amigos e desafie-os a superar seu desempenho!<br/><br/>
+
+                    🔥 Quão rápido você pode ser? Descubra em Tabuada Divertida! 💥
+                </h3>
+            }
             <br/>
             <br/>
             <Link to={`/jogo/` + tipo}>Jogar novamente</Link>
+            <Link to={`/ranking`}>Ranking</Link>
             <Link to="/">Início</Link>
         </div>
     )
