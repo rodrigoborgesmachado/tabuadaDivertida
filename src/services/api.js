79d84a7a5f480a930b399fa899f181b696e36106
Rodrigo Machado <rodrigoborgesmachado@gmail.com>
2024-04-23 12:33:19 -0300

    Adicionando referer no request.

--- src/services/api.js
+++ src/services/api.js
@@ -3,9 +3,13 @@ import axios from "axios";
 // URL da api: https://api.themoviedb.org/3/movie/now_playing?api_key=3afcecd6e3a27d3b4c7a657b515fc23f&language=en-US&page=1
 // base da url: https://api.themoviedb.org/3
 
+var apiUrl = window.location.href.includes("localhost") ? 'https://localhost:7119/api' : 'https://apisunsale.azurewebsites.net/api';
+
 const api = axios.create({
-    //baseURL: 'https://localhost:7119/api'
-    baseURL: 'https://apisunsale.azurewebsites.net/api'
+    baseURL: apiUrl,
+    headers: {
+        'Referer': 'https://www.tabuadadivertida.com/',
+    },
 })
 
 export default api;
