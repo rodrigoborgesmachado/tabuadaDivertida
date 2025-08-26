e2379ce0be2727be2503352bfbd442d2cf106eb0
Rodrigo Machado <rodrigo.m@condocontrol.com>
2025-08-26 12:00:03 -0300

    Adicionando conteúdo nas páginas de contato, política de privacidade e contato.

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,14 +1,15 @@
 import './style.css';
-import {Link} from 'react-router-dom';
 
 function Footer(){
     return(
         <footer>
             <h1><a target='_blank' rel='noreferrer' href='http://www.sunsalesystem.com.br/' >SunSale System</a></h1>
             <div className='links'>
-                <Link to='/privacidade'>Política de Privacidade</Link>
-                <Link to='/sobre'>Sobre</Link>
-                <Link to='/contato'>Contato</Link>
+                <h3>
+                    <a href='/privacidade'>Política de Privacidade</a>
+                    <a href='/sobre'>Sobre</a>
+                    <a href='/contato'>Contato</a>
+                </h3>
             </div>
         </footer>
     )
