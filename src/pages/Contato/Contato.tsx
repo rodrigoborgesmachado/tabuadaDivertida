51805baffe850d4f928e18880c984ee6d5d6feee
Rodrigo Machado <rodrigo.m@condocontrol.com>
2025-09-19 11:58:55 -0300

    Melhorando demais telas.

--- src/pages/Contato/Contato.tsx
+++ src/pages/Contato/Contato.tsx
@@ -1,22 +1,24 @@
 function Contato() {
     return (
         <div className="global-pageContainer-left">
-            <h1>Contato</h1>
-            <p className="global-mt">
-                Se você tiver dúvidas, sugestões ou encontrou algum problema no site <strong>Tabuada Divertida</strong>, entre em contato comigo! Terei o maior prazer em ajudar.
-            </p>
-            <p className="global-mt">
-                💌 <strong>Email:</strong> <a href="mailto:sunsalesystem@outlook.com">sunsalesystem@outlook.com</a>
-            </p>
-            <p className="global-mt">
-                Você também pode acompanhar atualizações e novos projetos pelo meu GitHub:
-            </p>
-            <p className="global-mt">
-                🛠️ <strong>GitHub:</strong> <a href="https://github.com/rodrigoborgesmachado" target="_blank" rel="noreferrer">github.com/rodrigoborgesmachado</a>
-            </p>
-            <p className="global-mt">
-                Obrigado por utilizar o Tabuada Divertida! 😊
-            </p>
+            <div className="contribute-container">
+                <h1>Contato</h1>
+                <p className="global-mt">
+                    Se você tiver dúvidas, sugestões ou encontrou algum problema no site <strong>Tabuada Divertida</strong>, entre em contato comigo! Terei o maior prazer em ajudar.
+                </p>
+                <p className="global-mt">
+                    💌 <strong>Email:</strong> <a href="mailto:sunsalesystem@outlook.com">sunsalesystem@outlook.com</a>
+                </p>
+                <p className="global-mt">
+                    Você também pode acompanhar atualizações e novos projetos pelo meu GitHub:
+                </p>
+                <p className="global-mt">
+                    🛠️ <strong>GitHub:</strong> <a href="https://github.com/rodrigoborgesmachado" target="_blank" rel="noreferrer">github.com/rodrigoborgesmachado</a>
+                </p>
+                <p className="global-mt">
+                    Obrigado por utilizar o Tabuada Divertida! 😊
+                </p>
+            </div>
         </div>
     );
 }
